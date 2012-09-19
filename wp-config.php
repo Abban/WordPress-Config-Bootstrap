e4d76f75fa5d78c2b3dbde87937521046939df8f
Ardy Dedase <ardy.dedase@gmail.com>
2012-09-19 10:54:10 +0800

    Separated different config files for different environments
    Refactored to use different config files for different environments.

--- wp-config.php
+++ wp-config.php
@@ -13,12 +13,13 @@
  */
 
 
-// Define Environments
+// Define Environments here except for production
+// Update this according to your needs
 $environments = array(
 	'local'       => '.local',
 	'development' => '.dev',
 	'staging'     => 'stage.',
-	'preview'     => 'preview.',
+	'preview'     => 'preview.'
 );
 
 // Get Server name
@@ -31,159 +32,21 @@ foreach($environments AS $key => $env){
 		define('ENVIRONMENT', $key);
 
 		break;
-
 	}
 }
 
-// If no environment is set default to production
+// If no environment is set, default to production
 if(!defined('ENVIRONMENT')) define('ENVIRONMENT', 'production');
 
-// Define different DB connection details depending on environment
-switch(ENVIRONMENT){
+define('ABSPATH', dirname(__FILE__) . '/');
 
-	case 'local':
+$env_config = ABSPATH . sprintf('wp-config-%s.php', ENVIRONMENT);
 
-		define('DB_NAME', 'bootstrap');
-		define('DB_USER', 'root');
-		define('DB_PASSWORD', 'root');
-		define('DB_HOST', 'localhost');
-		define('WP_DEBUG', true);
+// Terminate if file doesn't exist
+if(!file_exists(($env_config)))	exit("$env_config doesn't exist");
 
-		define('WP_SITEURL', 'http://bootstrap.local/');
-		define('WP_HOME', 'http://bootstrap.local/');
-
-		break;
-
-	case 'development':
-
-		define('DB_NAME', 'bootstrap');
-		define('DB_USER', 'root');
-		define('DB_PASSWORD', 'root');
-		define('DB_HOST', 'localhost');
-		define('WP_DEBUG', true);
-
-		break;
-
-	case 'staging':
-
-		define('DB_NAME', 'database_name_here');
-		define('DB_USER', 'username_here');
-		define('DB_PASSWORD', 'password_here');
-		define('DB_HOST', 'localhost');
-
-		break;
-
-	case 'preview':
-
-		define('DB_NAME', 'database_name_here');
-		define('DB_USER', 'username_here');
-		define('DB_PASSWORD', 'password_here');
-		define('DB_HOST', 'localhost');
-
-		break;
-
-	case 'mobile':
-
-		define('DB_NAME', 'database_name_here');
-		define('DB_USER', 'username_here');
-		define('DB_PASSWORD', 'password_here');
-		define('DB_HOST', 'localhost');
-
-		break;
-
-}
-
-// If batabase isn't defined then it will be defined here.
-// Put the details for your production environment in here.
-if(!defined('DB_NAME'))
-	define('DB_NAME', 'database_name_here');
-
-if(!defined('DB_USER'))
-	define('DB_USER', 'username_here');
-
-if(!defined('DB_PASSWORD'))
-	define('DB_PASSWORD', 'password_here');
-
-if(!defined('DB_HOST'))
-	define('DB_HOST', 'localhost');
-
-if(!defined('DB_CHARSET'))
-	define('DB_CHARSET', 'utf8');
-
-if(!defined('DB_COLLATE'))
-	define('DB_COLLATE', '');
-
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-
-if(!defined('AUTH_KEY'))
-	define('AUTH_KEY', 'put your unique phrase here');
-
-if(!defined('SECURE_AUTH_KEY'))
-	define('SECURE_AUTH_KEY', 'put your unique phrase here');
-
-if(!defined('LOGGED_IN_KEY'))
-	define('LOGGED_IN_KEY', 'put your unique phrase here');
-
-if(!defined('NONCE_KEY'))
-	define('NONCE_KEY', 'put your unique phrase here');
-
-if(!defined('AUTH_SALT'))
-	define('AUTH_SALT', 'put your unique phrase here');
-
-if(!defined('SECURE_AUTH_SALT'))
-	define('SECURE_AUTH_SALT', 'put your unique phrase here');
-
-if(!defined('LOGGED_IN_SALT'))
-	define('LOGGED_IN_SALT', 'put your unique phrase here');
-
-if(!defined('NONCE_SALT'))
-	define('NONCE_SALT', 'put your unique phrase here');
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-if(!isset($table_prefix)) $table_prefix  = 'wp_';
-
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-if(!defined('WPLANG'))
-	define('WPLANG', '');
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-if(!defined('WP_DEBUG'))
-	define('WP_DEBUG', false);
-
-/* That's all, stop editing! Happy blogging. */
-
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+/** Sets up WP config based on environment. */
+require_once($env_config);
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
